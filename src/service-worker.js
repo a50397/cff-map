d1a23c71fbda04f9d1c7e38500ab5606f0f42c70
Foka <stolcp@gmail.com>
2019-07-29 09:11:31 +0200

    service worker strategies tweaks

--- src/service-worker.js
+++ src/service-worker.js
@@ -7,6 +7,38 @@ self.__precacheManifest = [].concat(self.__precacheManifest || [])
 workbox.precaching.suppressWarnings()
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 
-workbox.routing.registerRoute(new RegExp('^https://data.sbb.ch/api/'), workbox.strategies.cacheFirst({
-  cacheName: 'api-cache'
-}))
+workbox.routing.registerRoute(
+  new RegExp('^https://data.sbb.ch/api/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'api-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://maps.googleapis.com/maps-api-v3/api/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'gapi-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+)
+
+workbox.routing.registerRoute(
+  new RegExp('^https://maps.googleapis.com/maps/'),
+  new workbox.strategies.CacheFirst({
+    cacheName: 'gmap-cache',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200]
+      })
+    ]
+  })
+)
